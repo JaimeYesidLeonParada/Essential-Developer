7046c32639b028505281af26b5f9aa86012550f7
Jaime Leon <jaime.leon@globant.com>
2022-09-05 12:16:41 -0500

    Create essential feed framework.

---
+++ Examples/EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Jaime Yesid Leon Parada on 5/09/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
