830e9b6272f44ffafc0e78ba2cc5b0f22c5aa94a
JaimeYesidLeonParada <ingenieroyesid@outlook.com>
2022-11-19 10:03:02 -0500

    Replace custom 'HTTPClientResult' enum with a nested typealias over the standar 'Swift.Result.'

--- Examples/EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
+++ Examples/EssentialFeed/EssentialFeed/FeedAPI/URLSessionHTTPClient.swift
@@ -16,10 +16,10 @@ public class URLSessionHTTPClient: HTTPClient {
     
     private struct UnexpectedValuesRepresentation: Error {}
     
-    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
         session.dataTask(with: url) {data, response, error in
             if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success(data, response))
+                completion(.success((data, response)))
             } else if let error = error {
                 completion(.failure(error))
             } else {
