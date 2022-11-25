1ec0c7ea5c379b9e241e7a0630f16d7022c9bf32
JaimeYesidLeonParada <ingenieroyesid@outlook.com>
2022-11-25 08:28:17 -0500

    mend

---
+++ Examples/EssentialFeedPrototype/Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Jaime Leon Parada on 25/11/22.
+//
+
+import UIKit
+
+class FeedViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+    }
+
+}
+
