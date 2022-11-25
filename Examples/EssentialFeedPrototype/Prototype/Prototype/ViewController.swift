b399f66ae2ce550e348ccfab8a01dc70ae72818c
JaimeYesidLeonParada <ingenieroyesid@outlook.com>
2022-11-25 07:24:58 -0500

    Add empty project for prototype.

---
+++ Examples/EssentialFeedPrototype/Prototype/Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Jaime Leon Parada on 25/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
