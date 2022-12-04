0720725daa1f467aa5ddeef691c0380dae6b03fe
JaimeYesidLeonParada <ingenieroyesid@outlook.com>
2022-12-04 09:27:36 -0500

    Completing the code not showed in the videos.

---
+++ Examples/EssentialFeed/EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Jaime Leon Parada on 4/12/22.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
--- Examples/EssentialFeed/EssentialFeediOS/FeedPresentation/FeedViewModel.swift
+++ Examples/EssentialFeed/EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -2,30 +2,11 @@
 //  FeedViewModel.swift
 //  EssentialFeediOS
 //
-//  Created by Jaime Leon Parada on 1/12/22.
+//  Created by Jaime Leon Parada on 4/12/22.
 //
 
-import Foundation
 import EssentialFeed
 
-final class FeedViewModel {
-    typealias Observer<T> = (T) -> Void
-    private let feedLoader: FeedLoader
-    
-    init(feedLoader: FeedLoader) {
-        self.feedLoader = feedLoader
-    }
-    
-    var onLoadingStateChange: Observer<Bool>?
-    var onFeedLoad: Observer<[FeedImage]>?
-    
-    func loadFeed() {
-        onLoadingStateChange?(true)
-        feedLoader.load { [weak self] result in
-            if let feed = try? result.get() {
-                self?.onFeedLoad?(feed)
-            }
-            self?.onLoadingStateChange?(false)
-        }
-    }
+struct FeedViewModel {
+    let feed: [FeedImage]
 }
