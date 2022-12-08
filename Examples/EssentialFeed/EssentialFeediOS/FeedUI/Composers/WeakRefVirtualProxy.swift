b6d56e026eb592b2efc31168b4cea3dac6f4ecfc
JaimeYesidLeonParada <ingenieroyesid@outlook.com>
2022-12-08 07:38:15 -0500

    Move the resto of classes of FeedUIComposer to own files.

---
+++ Examples/EssentialFeed/EssentialFeediOS/FeedUI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,29 @@
+//
+//  WeakRefVirtualProxy.swift
+//  EssentialFeediOS
+//
+//  Created by Jaime Leon Parada on 8/12/22.
+//
+
+import UIKit
+import EssentialFeed
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
